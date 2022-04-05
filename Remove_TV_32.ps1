f66b145b4547ad78d6e611797202a22af7426ed3
ctabora10 <36406819+ctabora10@users.noreply.github.com>
2022-04-05 16:21:27 -0500

    Add files via upload

---
+++ Remove_TV_32.ps1
@@ -0,0 +1,5 @@
+﻿$Uninstall32 = gci "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall" | foreach { gp $_.PSPath } | ? { $_ -match "TeamViewer Host" } | select UninstallString
+$uninstall32 = $uninstall32.UninstallString -Replace "msiexec.exe","" -Replace "/I","" -Replace "/X",""
+$uninstall32 = $uninstall32.Trim()
+#write-host $Uninstall32
+Start-Process "Msiexec.exe" -arg "/X $Uninstall32 /qn" -Wait
